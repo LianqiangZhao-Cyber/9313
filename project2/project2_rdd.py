a67e353efe88ed181b73f3d04e2886d62c72ed21
v_hmingzhao <v_hmingzhao@tencent.com>
2024-07-03 20:19:48 +0800

    rdd completed

--- project2/project2_rdd.py
+++ project2/project2_rdd.py
@@ -1,10 +1,13 @@
 from pyspark import SparkContext, SparkConf
 import sys
 import re
+from collections import defaultdict
 
 class Project2:   
         
     def run(self, inputPath, outputPath, stopwords, k):
+        invalid_line = defaultdict(int)
+        valid_line = defaultdict(int)
         # 1.Initialize SparkContext
         conf = SparkConf().setAppName("project2_rdd").setMaster("local")
         sc = SparkContext(conf=conf)
@@ -19,6 +22,7 @@ class Project2:
         
             # 2.2 load headline file
         headlines = sc.textFile(inputPath)
+
         
         # 3.Data preprocessing
         def preprocess(line):
@@ -33,8 +37,8 @@ class Project2:
             3. Ignore terms that contain non-alphabetical characters, you can use isalpha().
 
             4. Ignore the stop words such as "to", "the", "in", etc."""
-            category, headline = line.lower().split(",")
-            words = re.split(r"[\\s*$&#/\"',.:;?!\\[\\](){}<>~\\-_]+", headline)
+            category, headline = line.lower().split(",", 1)
+            words = re.split("[\\s*$&#/\"'\\,.:;?!\\[\\](){}<>~\\-_]+", headline)
             words = [word for word in words if word.isalpha() and word not in broadcast_stopwords.value]
             return (category, words)
             
@@ -47,17 +51,43 @@ class Project2:
                       for k in range(j+1, len(words))]
             return triads
         
-        traids_count = headlines.map(preprocess)\
-                                .flatMap(get_triad_frequency)\
+        # count of each triad
+        traids_count = headlines.map(preprocess).collect()
+        for line in traids_count:
+            if len(line[1])<3:
+                invalid_line[line[0]] += 1
+            else:
+                valid_line[line[0]] +=1 
+        traids_count = sc.parallelize(traids_count).flatMap(get_triad_frequency)\
                                 .map(lambda traid: (traid, 1))\
                                 .reduceByKey(lambda a, b: a+b)
+        
+        """# total count         
+        
+        category_counts = traids_count.map(lambda category: (category[0][0], category[1])) \
+                                      .reduceByKey(lambda a, b: a + b)"""
+        
+        valid_line = sc.parallelize(list(valid_line.items()))
+        # compute the relative frequency of each triad
+        # ('business', ('comeback', 'penney', 'wall'), 0.0010277492291880781)]
+        triad_relative_freq = traids_count.map(lambda x: (x[0][0], (x[0][1], x[1]))) \
+                                          .join(valid_line) \
+                                          .map(lambda x: (x[0], x[1][0][0], x[1][0][1] / x[1][1]))
+        
+        
+        
+        top_k_triads = triad_relative_freq.map(lambda x: (x[0], (x[1], x[2]))) \
+                                          .groupByKey()\
+                                          .flatMap(lambda x: [(x[0], triad[0], triad[1]) for triad in sorted(x[1], key=lambda y: -y[1])[:int(k)]]).sortBy(lambda x:x[0])
 
-            
-            
-            
         
         
-        
+        invalid_line_broadcast = sc.broadcast(dict(invalid_line.items()))
+        final_result = top_k_triads.map(lambda x: (x[0], f"{x[0]}\t{','.join(x[1])}:{x[2]}")) \
+                                .groupByKey() \
+                                .flatMap(lambda x: [f"{x[0]}\tinvalid line:{invalid_line_broadcast.value[x[0]]}"] + list(x[1]))\
+                                .sortBy(lambda x: x.split('\t')[0])
+        final_result.foreach(print)
 
         sc.stop()
 
